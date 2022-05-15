343c6c0bc87d5d0a3b412aabfca3c2b31fa437a1
Jonathan Ferreira <j0n4t@hotmail.com>
2022-05-15 17:29:41 -0300

    📦 build(ui): adiciona Chakra UI
    e limpa estilos padrão do Vite React

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
 import Todos from './pages/Todos';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
         <h1>Ebytr ToDo</h1>
       </header>
       <Todos />
--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { TodosProvider } from './context/TodosContext';
-import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <TodosProvider>
-      <App />
-    </TodosProvider>
+    <ChakraProvider>
+      <TodosProvider>
+        <App />
+      </TodosProvider>
+    </ChakraProvider>
   </React.StrictMode>,
 );
