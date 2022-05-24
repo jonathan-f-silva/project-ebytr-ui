6936fe461caa0502b9723a04fdfff0f0b8b57e60
Jonathan Ferreira <j0n4t@hotmail.com>
2022-05-23 22:14:01 -0300

    🔧 config(coverage): lcov
    Fonte: https://medium.com/@ocnvn/test-coverage-report-in-typescript-with-mocha-and-nyc-a6b10cbec24

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -13,6 +13,9 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/test/setup.ts',
+    coverage: {
+      reporter: ['text', 'lcov', 'html-spa'],
+    },
   },
   server: {
     proxy: {
