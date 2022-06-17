9998fca23e5961b9579f2be73f51f2378debfe5d
Jonathan Ferreira <j0n4t@hotmail.com>
2022-06-17 10:45:32 -0300

    🐛 fix: ajustes no import de tipos
    Referência: https://stackoverflow.com/questions/69051856/import-not-found-for-custom-types-in-vue-3-with-vite-vitesse

---
+++ src/utils/custom.ts
@@ -0,0 +1,4 @@
+import type { TodoField } from '../@types/custom';
+
+export const TODO_STATUSES = ['A fazer', 'Em andamento', 'Concluído! 🎉'];
+export const TODO_FIELDS: TodoField[] = ['createdAt', 'description', 'status'];
